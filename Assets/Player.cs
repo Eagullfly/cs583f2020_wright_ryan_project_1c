374932b27a490c9607b81137782885e9ad77a3f5
Ryan Wright <rwright3702@sdsu.edu>
2020-10-10 23:03:41 -0700

    Forked 1b as 1c and began roll character menu

---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string playerName;
+
+    public float strength;
+    public float dexterity;
+    public float constitution;
+    public float intelligence;
+    public float wisdom;
+    public float charisma;
+
+    public string race;
+    public string playerClass;
+
+    public string alignment;
+    public int maxXp;
+    public int currentXp;
+    public int armorClass;
+    public int walkingSpeed;
+    public int runningSpeed;
+    public int jumpHeight;
+
+    public List<string> languages;
+    public List<string> items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
